c47b58945d8f4fb5289ed538c21412698f2856e2
gamburgm <mitch.gamburg@gmail.com>
2019-07-06 17:37:40 -0400

    create pausable timer

--- popup.js
+++ popup.js
@@ -1,11 +1,33 @@
-function createTimer(time) {
-	let end     = new Date(Date.now() + time);
+const callback = (time) => {
+	let end = new Date(Date.now() + time);
+	let interval = setInterval(() => updateTime(end), 1000);
+	let timeRemaining = time;
+	let activeFunction;
 
-	setInterval(() => updateTime(end), 1000);
-}
+	const play = () => {
+		end = new Date(Date.now() + timeRemaining);
+		interval = setInterval(() => updateTime(end), 100);
+		activeFunction = pause;
+	}
+
+	const pause = () => {
+		timeRemaining = end.getTime() - Date.now();
+		clearInterval(interval);
+		activeFunction = play;
+	}
+
+	pause();
+
+	return () => activeFunction();
+};
 
 function updateTime(endTime) {
 	let time = endTime.getTime() - new Date().getTime();
+	
+	if (time <= 0) {
+		document.getElementById('timer').innerHTML = "You did it!";
+		return;
+	}
 
 	let seconds = Math.floor((time / 1000) % 60);
 	let minutes = Math.floor((time / (1000 * 60)) % 60);
@@ -16,10 +38,4 @@ function updateTime(endTime) {
 	document.getElementById('timer-seconds').innerHTML = ('0' + seconds).slice(-2);
 }
 
-document.getElementsByTagName('button')[0].addEventListener('click', function() {
-	alert("timer clicked, no result");
-});
-
-$(document).ready(() => {
-	createTimer(5*60*1000);
-});
+$('#help').click(callback(5*1000));
